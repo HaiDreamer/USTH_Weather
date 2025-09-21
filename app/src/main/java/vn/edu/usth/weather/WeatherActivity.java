cf1364b54a9f99c96a966a63f9a5dd5004d60f28
HaiDreamer <nghuyhai2004@gmail.com>
2025-09-21 10:51:51 +0700

    lab11 done

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -11,14 +11,18 @@ import android.widget.Button;
 import androidx.fragment.app.FragmentManager;
 
 import vn.edu.usth.weather.ui.theme.ForecastFragment;
+import android.media.MediaPlayer;
 
 public class WeatherActivity extends AppCompatActivity {
-    private String TAG = "WeatherActivity";     //help identify which class come form
+    private String TAG = "WeatherActivity";
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        mediaPlayer = MediaPlayer.create(this, R.raw.farout);
 
         Button weather_today = findViewById(R.id.weather_today);
         weather_today.setOnClickListener(v -> {
@@ -38,6 +42,9 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart");
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+        }
     }
 
     @Override
@@ -50,24 +57,39 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "onPause");
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
+        }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop");
+        releasePlayer();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
+        releasePlayer();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.i(TAG, "onRestart");
+    }
+
+    private void releasePlayer() {
+        if (mediaPlayer != null) {
+            try {
+                mediaPlayer.stop();
+            } catch (IllegalStateException ignored) {}
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 
 }
